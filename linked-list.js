472bd875d0a23c2929ef0f9fa8c83cb883b77628
unknown <hilow0090@gmail.com>
2024-01-31 20:32:56 -0800

    fixed issues

--- linked-list.js
+++ linked-list.js
@@ -35,13 +35,13 @@ class LinkedList {
 
   // returns node at index of ind
 
-  grab(ind) {
+  grab(idx) {
     if (idx >= this.length || idx < 0) {
       throw new Error("Invalid index.");
     }
     let i = 0;
     let j = this.head;
-    while (i !== ind) {
+    while (i !== idx) {
       j = j.next;
       i += 1;
     }
@@ -53,8 +53,8 @@ class LinkedList {
   unshift(val) {
     const node1 = new Node(val);
     if (!this.tail) {
-      this.tail = val;
-      this.head = val;
+      this.tail = node1;
+      this.head = node1;
     } else {
       node1.next = this.head;
       this.head = node1;
@@ -65,8 +65,15 @@ class LinkedList {
   /** pop(): return & remove last item. */
 
   pop() {
+    if (this.length === 1) {
+      const val = this.head.val;
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return val;
+    }
     const new_tail = this.grab(this.length - 2);
-    let val = new_tail.next.val;
+    let val = this.tail.val;
     this.tail = new_tail;
     new_tail.next = null;
     this.length -= 1;
@@ -76,26 +83,28 @@ class LinkedList {
   /** shift(): return & remove first item. */
 
   shift() {
-    let val = this.head.val;
-    this.head = this.head.next;
-    this.length -= 1;
+    const val = this.head.val;
     if (this.length < 2) {
-      this.head = this.tail;
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
     }
+    this.length -= 1;
     return val;
   }
 
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-    let node = grab(idx);
+    let node = this.grab(idx);
     return node.val;
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-    let node = grab(idx);
+    let node = this.grab(idx);
     node.val = val;
   }
 
@@ -104,7 +113,7 @@ class LinkedList {
   insertAt(idx, val) {
     if (idx === 0) {
       return this.unshift(val);
-    } else if (idx === this.length - 1) {
+    } else if (idx === this.length) {
       return this.push(val);
     }
     const node1 = new Node(val);
@@ -112,6 +121,7 @@ class LinkedList {
     let aft = this.grab(idx);
     prev.next = node1;
     node1.next = aft;
+    this.length += 1;
   }
 
   /** removeAt(idx): return & remove item at idx, */
@@ -136,6 +146,9 @@ class LinkedList {
   /** average(): return an average of all values in the list */
 
   average() {
+    if (this.length === 0) {
+      return 0;
+    }
     let cur = this.head;
     let i = 0;
     let sum = 0;
